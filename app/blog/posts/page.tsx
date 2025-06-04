0128f3405bdaf7b39597f5b3af7031affc5091be
Akshat <helloakshatsengar@gmail.com>
2025-06-04 19:21:26 +0530

    added posts

---
+++ app/blog/posts/page.tsx
@@ -0,0 +1,15 @@
+
+export const metadata = {
+  title: 'blog',
+  description: 'read my blog.',
+}
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">my blog.</h1>
+      <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+      <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+    </section>
+  )
+}
